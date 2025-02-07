ba6ae9ea637c8e240667f1bbffa3ddb4de817bcf
ancymon <60617648+ancym0n@users.noreply.github.com>
2025-02-07 17:34:52 +0100

    Page  - Test

--- src/python/manageAccounts.py
+++
@@ -1,5 +0,0 @@
-import os, subprocess
-script_dir = os.path.dirname(os.path.realpath(__file__))
-accounts_path = os.path.join(script_dir, "logs", "accounts.txt")
-subprocess.run(["notepad.exe", accounts_path])
-print("Opened accounts.txt\n > Path: "+accounts_path)
